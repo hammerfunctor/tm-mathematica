31aea87bb4737ff4181d236b10d7a6c6679bd684
hammerfunctor <topo20@protonmail.com>
2022-01-28 14:11:45 +0800

    complete: basic read-eval-print-loop functionality

--- progs/init-mma.scm
+++ progs/init-mma.scm
@@ -1,26 +1,42 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; MODULE      : init-mathematica.scm
-;; DESCRIPTION : Initialize mathematica plugin
-;; COPYRIGHT   : (C) 2005  Andrey Grozin
-;; COPYRIGHT   : (C) 2021  Hammer Hu
+;; MODULE      : init-mma.scm
+;; DESCRIPTION : Initialize the mma plugin
+;; COPYRIGHT   : (C) 2021 Hammer Hu
 ;;
 ;; This software falls under the GNU general public license version 3 or later.
 ;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
 ;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (mma-serialize lan t)
     (with u (pre-serialize lan t)
       (with s (texmacs->code (stree->tree u) "SourceCode")
-        (string-append s "\0"))))
+;;        (string-append s "\n<EOF>\n"))))
+        (string-append s "\n"))))
+
+
+(define (mma-entry)
+  (system-url->string
+    (if (url-exists? "$TEXMACS_HOME_PATH/plugins/mma/wolfram/tmWolfram.wls")
+       "$TEXMACS_HOME_PATH/plugins/mma/wolfram/tmWolfram.wls"
+       "$TEXMACS_PATH/plugins/mma/wolfram/tmWolfram.wls")))
+
+(define (mma-launcher)
+  (with boot (raw-quote (mma-entry))
+    (if (url-exists-in-path? "wolframscript")
+        (string-append "wolframscript " boot)
+        (string-append "wolframscript " boot))))
 
 (plugin-configure mma
-  (:require (url-exists? (url-unix "$PATH" "math")))
+  (:winpath "mma" "bin")
+  (:require (url-exists-in-path? "wolframscript"))
   (:serializer ,mma-serialize)
-  (:launch "tm_mma.bin")
-  (:session "Mathematica - CXX"))
+  (:launch ,(mma-launcher))
+  (:tab-completion #t)
+  (:session "mma"))
 
 (when (supports-mma?)
   (plugin-input-converters mma))
