b5980afbb690a088d6112883c94e4ef39dadb405
hammerfunctor <topo20@protonmail.com>
2023-02-10 23:10:44 -0500

    rename mma-math-converter.scm to mw-converter.scm

--- progs/mw-converter.scm
+++ progs/mw-converter.scm
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; MODULE      : mma-math-converter.scm
-;; DESCRIPTION : Convert string snippet in a math env
+;; MODULE      : mw-converter.scm
+;; DESCRIPTION : Convert string snippet in a math env to legal wolfram expression
 ;; COPYRIGHT   : (C) 2022 Hammer Hu
 ;;
 ;; This software falls under the GNU general public license version 3 or later.
@@ -10,21 +10,19 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(texmacs-module (mma-math-converter)
+(texmacs-module (mw-converter)
   (:use (ice-9 regex))
   )
 
-;; (use-modules (ice-9 regex))
-
-(define mma-symbol-map-table (make-hash-table 30))
-(hashq-set! mma-symbol-map-table 'sin (make-regexp "sin\\s*\\("))
-(hashq-set! mma-symbol-map-table 'cos (make-regexp "cos\\s*\\("))
-(hashq-set! mma-symbol-map-table 'tan (make-regexp "tan\\s*\\("))
-(hashq-set! mma-symbol-map-table 'cot (make-regexp "cot\\s*\\("))
-(hashq-set! mma-symbol-map-table 'sec (make-regexp "sec\\s*\\("))
-(hashq-set! mma-symbol-map-table 'csc (make-regexp "csc\\s*\\("))
-(hashq-set! mma-symbol-map-table 'exp (make-regexp "exp\\s*\\("))
-(hashq-set! mma-symbol-map-table 'log (make-regexp "log\\s*\\("))
+(define mw-symbol-map-table (make-hash-table 30))
+(hashq-set! mw-symbol-map-table 'sin (make-regexp "sin\\s*\\("))
+(hashq-set! mw-symbol-map-table 'cos (make-regexp "cos\\s*\\("))
+(hashq-set! mw-symbol-map-table 'tan (make-regexp "tan\\s*\\("))
+(hashq-set! mw-symbol-map-table 'cot (make-regexp "cot\\s*\\("))
+(hashq-set! mw-symbol-map-table 'sec (make-regexp "sec\\s*\\("))
+(hashq-set! mw-symbol-map-table 'csc (make-regexp "csc\\s*\\("))
+(hashq-set! mw-symbol-map-table 'exp (make-regexp "exp\\s*\\("))
+(hashq-set! mw-symbol-map-table 'log (make-regexp "log\\s*\\("))
 
 
 (define (find-balanced-br s idx br ibr level)
@@ -37,9 +35,9 @@
         (else (find-balanced-br s (+ idx 1) br ibr level))))
 
 ;; (string-replace "sin(x)/x" "Sin[" 0 4)
-(define (mma-map-single-param-func key funcname)
+(define (mw-map-single-param-func key funcname)
   (lambda (s)
-    (let ((matched (regexp-exec (hashq-ref mma-symbol-map-table key) s)))
+    (let ((matched (regexp-exec (hashq-ref mw-symbol-map-table key) s)))
       (if matched
           (let* ((start (match:start matched))
                  (end (match:end matched))
@@ -57,49 +55,49 @@
                 s))
           s))))
 
-(define mma-symbol-map-funcs
+(define mw-symbol-map-funcs
   (list
-   (mma-map-single-param-func 'sin "Sin")
-   (mma-map-single-param-func 'cos "Cos")
-   (mma-map-single-param-func 'tan "Tan")
-   (mma-map-single-param-func 'cot "Cot")
-   (mma-map-single-param-func 'sec "Sec")
-   (mma-map-single-param-func 'csc "Csc")
-   (mma-map-single-param-func 'exp "Exp")
-   (mma-map-single-param-func 'log "Log")
+   (mw-map-single-param-func 'sin "Sin")
+   (mw-map-single-param-func 'cos "Cos")
+   (mw-map-single-param-func 'tan "Tan")
+   (mw-map-single-param-func 'cot "Cot")
+   (mw-map-single-param-func 'sec "Sec")
+   (mw-map-single-param-func 'csc "Csc")
+   (mw-map-single-param-func 'exp "Exp")
+   (mw-map-single-param-func 'log "Log")
    ))
 
-(define (mma-symbol-map s)
-  (define (mma-symbol-map-rec restf s)
+(define (mw-symbol-map s)
+  (define (mw-symbol-map-rec restf s)
     (if (null? restf)
         s
         ;; recursive do the map
         (let ((mapped-string ((car restf) s)))
           ;; (display mapped-string)
           (if (string=? s mapped-string)
-              (mma-symbol-map-rec (cdr restf) s)
-              (mma-symbol-map-rec restf mapped-string)))))
-  (mma-symbol-map-rec mma-symbol-map-funcs s))
+              (mw-symbol-map-rec (cdr restf) s)
+              (mw-symbol-map-rec restf mapped-string)))))
+  (mw-symbol-map-rec mw-symbol-map-funcs s))
 
-(tm-define (mma-map-math lan wrapped rest)
+(tm-define (mw-map-math lan wrapped rest)
   (cond ((null? rest) wrapped)
         ((list? rest)
          (let ((firstel (car rest))) ;; depend on the first element
            (cond ((== firstel 'math)
                   ;; map the whole list: `rest'
                   (let* ((mathstring (plugin-math-input (list 'tuple lan (cadr rest))))
-                         (mapped-string (mma-symbol-map mathstring)))
+                         (mapped-string (mw-symbol-map mathstring)))
                     ;; (display mathstring)
                     ;; (display mapped-string)
                     mapped-string))
                  ((func? firstel 'math 1)
                   ;; map the first element of `rest': `firstel'
                   (let* ((mathstring (plugin-math-input (list 'tuple lan (cadr firstel))))
-                         (mapped-string (mma-symbol-map mathstring)))
+                         (mapped-string (mw-symbol-map mathstring)))
                     ;; (display mathstring)
                     ;; (display mapped-string)
-                    (mma-map-math lan (append wrapped (list mapped-string)) (cdr rest))))
-                 (else (mma-map-math lan (append wrapped (list firstel)) (cdr rest))))))
+                    (mw-map-math lan (append wrapped (list mapped-string)) (cdr rest))))
+                 (else (mw-map-math lan (append wrapped (list firstel)) (cdr rest))))))
         (else rest)))
 
 
